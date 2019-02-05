bce3e4a35631df8468d19e0487f8fbb0c340dc6d
Lucas Petti <luc_petti@hotmail.com>
2019-02-05 18:10:47 +0100

    created addFish and fish.js

--- catch-of-the-day/src/components/App.js
+++ catch-of-the-day/src/components/App.js
@@ -2,17 +2,48 @@ import React from 'react';
 import Header from './Header';
 import Order from './Order';
 import Inventory from './Inventory';
+import sampleFishes from '../sample-fishes';
+import Fish from './Fish';
 
 class App extends React.Component {
+  state = {
+    fishes: {},
+    order: {}
+  };
+
+  addFish = fish => {
+    const fishes = { ...this.state.fishes };
+    fishes[`fish${Date.now()}`] = fish;
+    // this.state.fishes.fish1 = fish;
+    // console.log('Adding your fish!');
+    this.setState({ fishes });
+  };
+
+  loadSampleFishes = () => {
+    this.setState({ fishes: sampleFishes });
+  };
+
   render() {
     return (
       <div className="catch-of-the-day">
         <div className="menu">
           <Header tagline="Fresh Seafood Market" age={500}/>
-        { /* <Fish /> */ }
+          <ul className="fishes">
+            {Object.keys(this.state.fishes).map(key => (
+              <Fish
+                key={key}
+                index={key}
+                details={this.state.fishes[key]}
+                addToOrder={this.addToOrder}
+              />
+            ))}
+          </ul>
         </div>
         <Order />
-        <Inventory />
+        <Inventory
+          addFish={this.addFish}
+          loadSampleFishes={this.loadSampleFishes}
+        />
       </div>
     )
   }
--- catch-of-the-day/src/components/StorePicker.js
+++ catch-of-the-day/src/components/StorePicker.js
@@ -1,17 +1,28 @@
 import React from 'react';
+import { getFunName } from "../helpers";
 
 class StorePicker extends React.Component {
+  myInput = React.createRef();
+
+  goToStore = event => {
+    event.preventDefault();
+    console.log(this.myInput.current.value);
+    const storeName = this.myInput.current.value;
+    this.props.history.push(`/store/${storeName}`);
+  }
   render() {
     return (
-      <>
-        <p>Fishes!</p>
-        <form className="store-selector">
-          <h2>Please enter a store</h2>
-          <input type="text" required placeholder="Store Name" />
-          <button type="submit">Visit Store</button>
-        </form>
-      </ >
-      )
+      <form className="store-selector" onSubmit={this.goToStore}>
+        <h2>Please enter a store</h2>
+        <input
+          type="text"
+          ref={this.myInput}
+          required placeholder="Store Name"
+          defaultValue={getFunName()}
+        />
+        <button type="submit">Visit Store</button>
+      </form>
+    );
   }
 }
 
