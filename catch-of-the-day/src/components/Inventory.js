3e0e30e9ce557238caef87d96f7862e0ac786d5c
Lucas Petti <luc_petti@hotmail.com>
2019-02-18 09:17:28 +0100

    started login logic

--- catch-of-the-day/src/components/Inventory.js
+++ catch-of-the-day/src/components/Inventory.js
@@ -1,7 +1,10 @@
 import React from "react";
 import PropTypes from "prop-types";
+import firebase from "firebase";
 import AddFishForm from "./AddFishForm";
+import Login from "./Login";
 import EditFishForm from "./EditFishForm";
+import base, { firebaseApp } from "../base";
 
 class Inventory extends React.Component {
   static propTypes = {
@@ -11,7 +14,21 @@ class Inventory extends React.Component {
     loadSampleFishes: PropTypes.func,
     addFish: PropTypes.func
   };
+
+  authHandler = async authData => {
+    console.log(authData);
+  }
+
+  authenticate = provider => {
+    const authProvider = new firebase.auth[`${provider}AuthProvider`]();
+    firebaseApp
+      .auth()
+      .signInWithPopup(authProvider)
+      .then(this.authHandler);
+  };
+
   render() {
+    return (<Login authenticate={this.authenticate} />)
     return (
       <div className="inventory">
         <h2>Inventory</h2>
